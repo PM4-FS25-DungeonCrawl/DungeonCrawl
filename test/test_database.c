e06bb0bb4825e95e9a8ba82827299e5f65b29b7e
baranjak <ja.baranec@gmail.com>
2025-03-27 18:08:16 +0100

    add database and test

---
+++ test/test_database.c
@@ -0,0 +1,14 @@
+//
+// Created by jabar on 27.03.2025.
+//
+#include <stdio.h>
+#include "../database/database.h"
+#include <assert.h>
+
+
+void test_db_open(){
+    DBConnection db_connection;
+    assert(db_open(&db_connection, "test.db") == 1);
+    db_close(&db_connection);
+    printf("Test_db_open passed\n");
+}
