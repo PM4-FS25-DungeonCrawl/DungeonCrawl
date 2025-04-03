f439ff1f110c76bd31a2cc5c7daa3288168f25b7
Nino <nino.frei@gmail.com>
2025-04-03 16:20:03 +0200

    fix indent & prepare COMBAT_MODE State

--- src/game.c
+++ src/game.c
@@ -6,9 +6,8 @@
 #include "combat/ability.h"
 #include "combat/npc.h"
 #include "combat/pc.h"
+#include "combat/combat_mode.h"
 
-#include "character_stats.h"
-#include "combat_mode.h"
 #include "map/map_mode.h"
 #include "map/map_generator.h"
 #include "../include/termbox2.h"
@@ -22,10 +21,6 @@ typedef enum {
     EXIT
 } game_state_t;
 
-int add(int a, int b) {
-    return a + b;
-}
-
 int init_game(){
     if (tb_init() != 0) {
         log_msg(ERROR, "Game", "Failed to initialize termbox");
@@ -33,8 +28,7 @@ int init_game(){
     }
     tb_set_output_mode(TB_OUTPUT_NORMAL);
 
-    log_msg(INFO, "Game", "game loop starting");
-    bool running = true;
+    bool running = true; //should only be set in the state machine
     game_state_t current_state = GENERATE_MAP;
 
     init_map_mode();
@@ -46,15 +40,16 @@ int init_game(){
     if (ability_table == NULL || goblin == NULL || player == NULL || healingPotion == NULL) {
         log_msg(ERROR, "Game", "Failed to initialize game components");
         current_state = EXIT;
+    } else {
+        // add abilities to player and goblin
+        add_ability_to_character(goblin->base, &ability_table->table[BITE]);
+        add_ability_to_character(player->base, &ability_table->table[FIREBALL]);
+        add_ability_to_character(player->base, &ability_table->table[SWORD_SLASH]);
+
+        //add healing potion to player
+        add_usable_item_to_player(player, healingPotion);
+        log_msg(INFO, "Game", "game loop starting");
     }
-
-    // add abilities to player and goblin
-    add_ability_to_character(goblin->base, &ability_table->table[BITE]);
-    add_ability_to_character(player->base, &ability_table->table[FIREBALL]);
-    add_ability_to_character(player->base, &ability_table->table[SWORD_SLASH]);
-
-    //add healing potion to player
-    add_item_to_player(player, healingPotion);
 
     while (running) {
         switch (current_state) {
@@ -65,18 +60,34 @@ int init_game(){
                 current_state = MAP_MODE;
                 break;
             case MAP_MODE:
+                // TODO: should change the state of the state machine, when walking into a monster
                 if (map_mode_update()) {
                     current_state = EXIT;
                 }
                 break;
             case COMBAT_MODE: {
-
-
-                // current_state = (combat(&player, &monster))? MAP_MODE : EXIT;
+                switch (start_combat(player, goblin)) {
+                    case PLAYER_WON:
+                        log_msg(FINE, "Game", "Player won the combat");
+                        // TODO: add loot to player
+                        // TODO: delete goblin from map
+                        current_state = MAP_MODE;
+                        break;
+                    case PLAYER_LOST:
+                        log_msg(FINE, "Game", "Player lost the combat");
+                        //TODO: instead of exiting the game, a death screen should be shown
+                        current_state = EXIT;
+                        break;
+                    case EXIT_GAME:
+                        current_state = EXIT;
+                        break;
+                }
                 break;
             }
             case EXIT:
                 free_ability_table(ability_table);
+                free_goblin(goblin);
+                free_player(player);
                 close_log_file(1);
                 running = false;
                 break;
