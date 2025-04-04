2681eec31783483c7aa01256ac849271e1969ccd
Nino <nino.frei@gmail.com>
2025-04-04 10:24:15 +0200

    rm combat_common and implement a common.h file

--- src/combat/combat_common.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMBAT_COMMON_H
-#define COMBAT_COMMON_H
-
-#define MAX_STRING_LENGTH 50
-
-#endif //COMBAT_COMMON_H
---
+++ src/common.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define MAX_NAME_LENGTH 50
+
+
+#endif //COMMON_H
