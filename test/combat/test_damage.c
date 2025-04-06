3927dc530b50a393db97a1c185dd26b05d8f53f1
Nino <nino.frei@gmail.com>
2025-04-06 16:01:27 +0200

    fix damage test

--- test/combat/test_damage.c
+++ test/combat/test_damage.c
@@ -1,6 +1,6 @@
-#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "../src/combat/ability.h"
 #include "../src/character/character.h"
@@ -9,47 +9,45 @@
 #include "../src/combat/combat_mode.h"
 
 
+// allocate in data segment
+character_t test_character = {
+    .type = PLAYER,
+    .name = "Test Character",
+    .health = 100,
+    .armor = 0,
+    .might = 0,
+    .deflection = 0,
+    .fortitude = 0,
+    .will = 0,
+    .resistance = {0},
+    .abilities = NULL,
+    .ability_count = 0,
+    .items = NULL,
+    .item_count = 0
+};
+ability_t test_ability = {
+    .name = "Test Ability",
+    .roll_amount = 3,
+    .accuracy = 100,
+    .dice_size = D6,
+    .damage_type = PHYSICAL
+};
+
 void test_roll_hit() {
-    // allocate memory for the test objects
-    character_t* test_character = malloc(sizeof(character_t));
-    ability_t* test_ability = malloc(sizeof(ability_t));
 
-    test_character->health = 100; // set health
-    test_character->armor = 0; // set armor
+    // Test with high accuracy ability (should hit)
+    assert(roll_hit(&test_character, &test_ability) == true);
 
-    test_ability->roll_amount = 3;
-    test_ability->accuracy = 100;
-    test_ability->dice_size = D6;
-    test_ability->damage_type = PHYSICAL;
+    // Test with low accuracy ability (should miss)
+    test_ability.accuracy = 0;
+    test_character.deflection = 20;
+    assert(roll_hit(&test_character, &test_ability) == false);
 
-
-
-    // Test with high accuracy ability (should always hit)
-    if (roll_hit(test_character, test_ability) != true) {
-        free(test_ability);
-        free(test_character);
-        assert(false);
-    }
-
-    // Test with low accuracy ability (should miss often)
-    test_ability->accuracy = 0;
-    test_character->deflection = 20;
-    if (roll_hit(test_character, test_ability) != false) {
-        free(test_ability);
-        free(test_character);
-        assert(false);
-    }
-
-    free(test_ability);
-    free(test_character);
     printf("test_roll_hit passed!\n");
 }
 
 void test_roll_damage() {
-    ability_t* test_ability = malloc(sizeof(ability_t));
-
-    const int damage = roll_damage(test_ability);
-    free(test_ability); // free memory before assert
+    const int damage = roll_damage(&test_ability);
 
     // The result should be between 3 (minimum) and 18 (maximum)
     assert(damage >= 3 && damage <= 18);
@@ -57,17 +55,13 @@ void test_roll_damage() {
 }
 
 void test_deal_damage_to_armor() {
-    character_t* test_player = malloc(sizeof(character_t));
-    test_player->health = 100;
-    test_player->armor = 10; // set armor
-
-
-    deal_damage(test_player, PHYSICAL, 50);
-    const int new_health = test_player->health;
-    free(test_player); // free memory before assert
+    test_character.health = 100;
+    test_character.armor = 10; // set armor
 
     // Damage after armor reduction: 50 - 10 = 40
-    assert(new_health == 60);
+    deal_damage(&test_character, PHYSICAL, 50);
+    assert(test_character.health == 60); // 100 - (50 - 10) = 60
+
     printf("test_deal_damage_to_armor passed!\n");
 }
 
