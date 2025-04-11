17168d6a3cb0cdaeb04006057343a6afbf0b0a87
baranjak <ja.baranec@gmail.com>
2025-04-11 11:08:20 +0200

    small changes

--- src/local/local.h
+++ src/local/local.h
@@ -22,7 +22,7 @@ typedef void (*update_observer_t)(local_language_t lang);
 void init_local(void);
 
 char* get_local_string(const char* key);
-bool set_language(const language_t lang);
+bool set_language(const local_language_t local_lang);
 
 void add_observer(update_observer_t update_func);
 
