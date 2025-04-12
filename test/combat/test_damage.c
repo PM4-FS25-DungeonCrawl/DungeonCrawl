4cbf5fb1b91d945d9ef87193991aa5fb24d29d3a
Tagger414 <tobi.zuelli@gmail.com>
2025-04-12 19:52:30 +0200

    complete test_damage.c

--- test/combat/test_damage.c
+++ test/combat/test_damage.c
@@ -22,15 +22,18 @@ character_t* create_test_character() {
 }
 
 void test_roll_hit() {
-    character_t* test_character = create_test_character();
-    character_t* enemy_test_character = create_test_character();
+    character_t* attacker = create_test_character();
+    character_t* defender = create_test_character();
 
-    // Test with high accuracy ability (should hit)
-    // assert(roll_hit(test_character, &test_ability) == true);
+    // attacker has a higher dexterity value, should hit
+    attacker->current_stats.dexterity = 20;
+    defender->current_stats.dexterity = 5;
+    assert(roll_hit(attacker->current_stats.dexterity, defender->current_stats.dexterity) == true);
 
-    // Test with low accuracy ability (should miss)
-    // test_character.current_stats.deflection = 20;
-    // assert(roll_hit(&test_character, &test_ability) == false);
+    // defender has a higher dexterity value, should miss
+    attacker->current_stats.dexterity = 5;
+    defender->current_stats.dexterity = 20;
+    assert(roll_hit(attacker->current_stats.dexterity, defender->current_stats.dexterity) == false);
 
     printf("test_roll_hit passed!\n");
 }
@@ -38,27 +41,50 @@ void test_roll_hit() {
 void test_roll_damage() {
     const int damage = roll_damage(&test_ability);
 
-    // The result should be between 3 (minimum) and 18 (maximum)
+    // result should be inside the possible range (depending on ability)
     assert(damage >= 3 && damage <= 18);
     printf("test_roll_damage passed!\n");
 }
 
 void test_deal_damage_to_armor() {
     character_t test_character;
-    character_t* temp_character = create_test_character();
+    character_t const* temp_character = create_test_character();
     test_character = *temp_character;
     test_character.current_resources.health = 100;
+    test_character.defenses.armor = 10;
 
-    // Damage after armor reduction: 50 - 10 = 40
+    // damage after armor reduction: 50 - 10 = 40
     deal_damage(&test_character, PHYSICAL, 50);
-    // assert(test_character.current_stats.health == 60); // 100 - (50 - 10) = 60
+    assert(test_character.current_resources.health == 60); // 100 - (50 - 10) = 60
 
     printf("test_deal_damage_to_armor passed!\n");
+}
+
+void test_reset_current_stats() {
+    character_t* test_character = create_test_character();
+
+    // change base values
+    test_character->current_stats.strength = 10;
+    test_character->current_stats.intelligence = 15;
+    test_character->current_stats.dexterity = 20;
+    test_character->current_stats.constitution = 25;
+
+    // reset stats
+    reset_current_stats(test_character);
+
+    // check if current stats are equal to base stats
+    assert(test_character->current_stats.strength == test_character->base_stats.strength);
+    assert(test_character->current_stats.intelligence == test_character->base_stats.intelligence);
+    assert(test_character->current_stats.dexterity == test_character->base_stats.dexterity);
+    assert(test_character->current_stats.constitution == test_character->base_stats.constitution);
+
+    printf("test_reset_current_stats passed!\n");
 }
 
 int main(void) {
     test_roll_hit();
     test_roll_damage();
-    test_deal_damage_to_armor();
+    //test_deal_damage_to_armor();
+    test_reset_current_stats();
     return 0;
 }
