8ce4a9ed3712cd07f3b00ffc6c5020cca614b45a
Odermatt Dominic (odermdo1) <odermdo1@students.zhaw.ch>
2025-04-17 13:36:52 +0200

    draw_light refactor

--- src/map/draw/draw_light.c
+++ src/map/draw/draw_light.c
@@ -1,4 +1,5 @@
 #include "draw_light.h"
+#include "../map.h"
 
 #include <stdlib.h>
 
@@ -11,7 +12,7 @@
  * @param prev_wall_at pointer to variable, which should be written
  * @return 1 if loops must break, 0 when not
  */
-int need_loop_break(const int x, const int y, const vector2d_t dir, int j, int* prev_wall_at) {
+int need_loop_break(const int x, const int y, const vector2d_t dir, int j, int *prev_wall_at) {
     if (j == 0) {
         //gets the x or y value of the calculated coordinates
         *prev_wall_at = abs(y * dir.dy + x * dir.dx);
@@ -33,28 +34,129 @@ int need_loop_break(const int x, const int y, const vector2d_t dir, int j, int* 
  * - the reverse check vector
  */
 const vector2d_t checks_vector[4][2] = {
-        {{1, 1}, {1, 0}},   // for up
-        {{-1, -1}, {-1, 0}},// for down
-        {{1, -1}, {0, -1}}, // for left
-        {{-1, 1}, {0, 1}},  // for right
+    {{1, 1}, {1, 0}}, // for up
+    {{-1, -1}, {-1, 0}}, // for down
+    {{1, -1}, {0, -1}}, // for left
+    {{-1, 1}, {0, 1}}, // for right
 };
 
+
 /**
- * Draws the light around the player in arr2.
- * The algorithmen reveals the map around the player, based on the arr1, in arr2.
+ * This function processes the tile at the given coordinates and updates the revealed_map array.
+ * @param map original map tile array
+ * @param revealed_map the array to be updated
+ * @param x current tile x-coordinates
+ * @param y current tile y-coordinates
+ * @param HEIGHT the HEIGHT of the map
+ * @param prev_wall_at index of the previous wall tile
+ * @param dir the direction to check
+ * @param j loop counter
+ * @return
+ */
+int process_tile(int x, int y, int *prev_wall_at, const vector2d_t dir, int j) {
+    int break_loop = 0;
+
+    switch (map[x][y]) {
+        case WALL:
+            revealed_map[x][y] = WALL;
+            break_loop = need_loop_break(x, y, dir, j, prev_wall_at);
+            break;
+        case FLOOR:
+            revealed_map[x][y] = FLOOR;
+            break;
+        case START_DOOR:
+            revealed_map[x][y] = START_DOOR;
+            break;
+        case EXIT_DOOR:
+            revealed_map[x][y] = EXIT_DOOR;
+            break;
+        case KEY:
+            revealed_map[x][y] = KEY;
+            break;
+        case GOBLIN:
+            revealed_map[x][y] = GOBLIN;
+            break;
+        default:
+            // Does nothing
+            break;
+    }
+    return break_loop;
+}
+
+int check_and_process_tile(int x, int y, int *prev_wall_at, const vector2d_t dir, int j,
+                           const vector2d_t diagonal_check, const vector2d_t reverse_check) {
+    int quit = 0;
+
+    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
+        //calculated x or y is out of bounds
+        quit = 1;
+    } else {
+        if (revealed_map[x][y] == HIDDEN) {
+            //initialize the relative diagonal and reverse tiles based on the y and x values
+            const int rel_diagonal = map[x + diagonal_check.dx][y + diagonal_check.dy];
+            const int rel_reverse = map[x + reverse_check.dx][y + reverse_check.dy];
+
+            if (rel_diagonal == WALL && rel_reverse == WALL && j > 1) {
+                //if the diagonal and reverse tiles are walls, and the distance from the player is greater than 1
+                //then the tile must be hidden because reverse tile is blocking the view
+                quit = 1;
+            } else {
+                quit = process_tile(x, y, prev_wall_at, dir, j);
+            }
+        } else if (revealed_map[x][y] == WALL && need_loop_break(x, y, dir, j, prev_wall_at)) {
+            quit = 1;
+        }
+    }
+    return quit;
+}
+
+
+/**
+ * Processes light in a specific direction and updates the revealed map.
+ * @param player the player's position on the map
+ * @param dir the direction vector
+ * @param diagonal_check the diagonal check vector
+ * @param reverse_check the reverse check vector
+ */
+void process_light_in_direction(const vector2d_t player, const vector2d_t dir, const vector2d_t diagonal_check,
+                                const vector2d_t reverse_check, const int light_radius) {
+    int correction = 0;
+    int prev_wall_at = -1;
+
+    for (int j = 0; j <= light_radius; j++) {
+        const int start_x = player.dx + j * dir.dy;
+        int start_y = player.dy + j * dir.dx;
+
+        if (dir.dx != 0) {
+            //if the direction is vertical negate the subtrahend
+            start_y = player.dy - j * dir.dx;
+        }
+
+        if (start_x < 0 || start_x >= WIDTH || start_y < 0 || start_y >= HEIGHT) {
+            //start position is out of bounds, skip to the next direction
+            break;
+        }
+        for (int k = 1; k <= light_radius - correction; k++) {
+            const int x = start_x + k * dir.dx;
+            const int y = start_y + k * dir.dy;
+
+            if (check_and_process_tile(x, y, &prev_wall_at, dir, j, diagonal_check, reverse_check)) {
+                break;
+            }
+        }
+        correction++;
+    }
+}
+
+/**
+ * Draws the light around the player in revealed_map.
+ * The algorithm reveals the map around the player, based on the map, in revealed_map.
  * The light radius is the distance from the player, where the map tile is still visible.
  * A light radius of 3 means 3 tiles are visible in each direction (up, down, left, right) from the player.
  *
- * @param arr1 the pointer to the 2D array containing all the map tiles (no Hidden tiles)
- * @param arr2 the pointer to the 2D array to reveal the arr1,
- * based on the player's position and light radius
- * @param height the height of the map
- * @param width the width of the map
  * @param player the player's position on the map
- * @param light_radius the radius of the light around the player
  */
-void draw_light_on_player(const map_tile_t* arr1, map_tile_t* arr2, const int height, const int width,
-                          const vector2d_t player, const int light_radius) {
+void draw_light_on_player(const vector2d_t player, const int light_radius) {
     if (light_radius <= 0) {
         //light radius is negative or 0, do nothing
         return;
@@ -65,82 +167,6 @@ void draw_light_on_player(const map_tile_t* arr1, map_tile_t* arr2, const int he
         const vector2d_t diagonal_check = checks_vector[i][0];
         const vector2d_t reverse_check = checks_vector[i][1];
 
-        int correction = 0;
-        int prev_wall_at = -1;
-
-        for (int j = 0; j <= light_radius; j++) {
-            const int start_x = player.dx + j * dir.dy;
-            int start_y = player.dy + j * dir.dx;
-
-            if (dir.dx != 0) {
-                //if the direction is vertical negate the subtrahend
-                start_y = player.dy - j * dir.dx;
-            }
-
-            if (start_x < 0 || start_x >= width || start_y < 0 || start_y >= height) {
-                //start position is out of bounds, skip to the next direction
-                break;
-            }
-            for (int k = 1; k <= light_radius - correction; k++) {
-                const int x = start_x + k * dir.dx;
-                const int y = start_y + k * dir.dy;
-                if (x < 0 || x >= width || y < 0 || y >= height) {
-                    //calculated x or y is out of bound
-                    break;
-                }
-
-                //calculated access index
-                const int access_idx = x * height + y;
-
-                if (arr2[access_idx] == HIDDEN) {
-                    //initialize the relative diagonal and reverse tiles based on the y and x values
-                    const int rel_diagonal = arr1[(x + diagonal_check.dx) * height + y + diagonal_check.dy];
-                    const int rel_reverse = arr1[(x + reverse_check.dx) * height + y + reverse_check.dy];
-
-                    if (rel_diagonal == WALL && rel_reverse == WALL && j > 1) {
-                        //if the diagonal and reverse tiles are walls, and the distance from the player is greater than 1
-                        //then the tile must be hidden because reverse tile is blocking the view
-                        break;
-                    }
-
-                    // if set to 1 break loop
-                    int break_loop = 0;
-
-                    switch (arr1[access_idx]) {
-                        case WALL:
-                            arr2[access_idx] = WALL;
-
-                            if (need_loop_break(x, y, dir, j, &prev_wall_at)) {
-                                break_loop = 1;
-                            }
-                            break;
-                        case FLOOR:
-                            arr2[access_idx] = FLOOR;
-                            break;
-                        case START_DOOR:
-                            arr2[access_idx] = START_DOOR;
-                            break;
-                        case EXIT_DOOR:
-                            arr2[access_idx] = EXIT_DOOR;
-                            break;
-                        case KEY:
-                            arr2[access_idx] = KEY;
-                            break;
-                        case GOBLIN:
-                            arr2[access_idx] = GOBLIN;
-                            break;
-                        default:
-                            //does nothing
-                            break;
-                    }
-                    if (break_loop) {
-                        break;
-                    }
-                } else if (arr2[access_idx] == WALL && need_loop_break(x, y, dir, j, &prev_wall_at)) {
-                    break;
-                }
-            }
-            correction++;
-        }
+        process_light_in_direction(player, dir, diagonal_check, reverse_check, light_radius);
     }
 }
