06b1f51d9e583d8d2c5013916ff40836fba7fd60
jilzerndt <jil.zerndt@outlook.com>
2025-04-21 07:11:00 +0200

    final cleanup main_menu

--- src/menu/main_menu.c
+++ src/menu/main_menu.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <time.h>// For nanosleep
 
+bool menu_active;
+
 void init_main_menu(void) {
     log_msg(INFO, "Menu", "Initializing main menu");
 }
@@ -36,7 +38,7 @@ menu_result_t show_main_menu(bool game_in_progress) {
 
     int selected_index = 0;
     menu_result_t result = MENU_CONTINUE;
-    bool menu_active = true;
+    menu_active = true;
 
     while (menu_active) {
         draw_menu(menu_options, menu_count, selected_index);
@@ -53,37 +55,9 @@ menu_result_t show_main_menu(bool game_in_progress) {
                 selected_index = (selected_index + 1) % menu_count;
                 break;
             case TB_KEY_ENTER:
-                if (strcmp(menu_options[selected_index], NEW_GAME_OPTION) == 0) {
-                    if (game_in_progress && !show_confirmation("Do you want to continue?")) {
-                        // User declined, continue menu loop
-                        continue;
-                    }
-                    result = MENU_START_GAME;
-                    menu_active = false;
-                } else if (strcmp(menu_options[selected_index], CONTINUE_OPTION) == 0) {
-                    result = MENU_CONTINUE;
-                    menu_active = false;
-                } else if (strcmp(menu_options[selected_index], SAVE_GAME_OPTION) == 0) {
-                    log_msg(INFO, "Main Menu", "Calling show_save_game_menu");
-                    result = show_save_game_menu();
-                    log_msg(INFO, "Main Menu", "Returned from show_save_game_menu with result: %d", result);
-                    if (result == MENU_SAVE_GAME) {
-                        menu_active = false;
-                    }
-                } else if (strcmp(menu_options[selected_index], LOAD_GAME_OPTION) == 0) {
-                    result = show_load_game_menu(game_in_progress);
-                    if (result == MENU_LOAD_GAME) {
-                        menu_active = false;
-                    }
-                } else if (strcmp(menu_options[selected_index], EXIT_OPTION) == 0) {
-                    if (game_in_progress && !show_confirmation("Do you want to exit?")) {
-                        // User declined, continue menu loop
-                        continue;
-                    }
-
-                    result = MENU_EXIT;
-                    menu_active = false;
-                }
+                // Get the selected menu option
+                const char* selected_option = menu_options[selected_index];
+                result = select_menu_option(selected_option, game_in_progress);
                 break;
             case TB_KEY_CTRL_C:
                 result = MENU_EXIT;
@@ -99,5 +73,44 @@ menu_result_t show_main_menu(bool game_in_progress) {
         }        
     }
 
+    return result;
+}
+
+menu_result_t select_menu_option(const char* selected_option, bool game_in_progress) {
+    menu_result_t result = MENU_CONTINUE;
+    log_msg(INFO, "Main Menu", "Selected option: %s", selected_option);
+
+    switch(selected_option[0]) {
+        case 'N': // New Game (first letter 'N')
+            if (!game_in_progress || show_confirmation("Do you want to continue?")) {
+                result = MENU_START_GAME;
+                menu_active = false;
+            } 
+            break;
+        case 'C': // Continue (first letter 'C')
+            result = MENU_CONTINUE;
+            menu_active = false;
+            break;
+        case 'S': // Save Game (first letter 'S')
+            log_msg(INFO, "Main Menu", "Calling show_save_game_menu");
+            result = show_save_game_menu();
+            log_msg(INFO, "Main Menu", "Returned from show_save_game_menu with result: %d", result);
+            if (result == MENU_SAVE_GAME) {
+                menu_active = false;
+            }
+            break;
+        case 'L': // Load Game (first letter 'L')
+            result = show_load_game_menu(game_in_progress);
+            if (result == MENU_LOAD_GAME) {
+                menu_active = false;
+            }
+            break;
+        case 'E': // Exit (first letter 'E')
+            if (!game_in_progress || show_confirmation("Do you want to exit?")) {
+                result = MENU_EXIT;
+                menu_active = false;
+            }
+            break;
+    }
     return result;
 }
