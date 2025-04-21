328c34584c694c7309ddb82be43bca9d09aaafb4
jilzerndt <jil.zerndt@outlook.com>
2025-04-21 14:06:08 +0200

    partial refactor of game.c

--- src/game.h
+++ src/game.h
@@ -4,6 +4,26 @@
 #define COLOR_FOREGROUND TB_WHITE
 #define COLOR_BACKGROUND TB_BLACK
 
+typedef enum {
+    MAIN_MENU,
+    MAP_MODE,
+    COMBAT_MODE,
+    GENERATE_MAP,
+    EXIT,
+    EXIT_WITH_ERROR
+} game_state_t;
+
+typedef enum {
+    SUCCESS = 0,
+    FAIL_TB_INIT = 1,
+    FAIL_LOCAL_INIT = 2,
+    FAIL_GAME_ENTITY_INIT = 3,
+} exit_code_t;
+
 int init_game();
+
+void main_menu_state();
+
+void map_mode_state();
 
 #endif// GAME_H
