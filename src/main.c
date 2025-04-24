958af32fbdfaee820a2f4d664a7436ce3f21ace1
Nino <nino.frei@gmail.com>
2025-04-24 09:26:09 +0200

    Refactor main.c: update memory pool variable naming and usage for consistency

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@
 
 #include <time.h>
 
-// === internal global variables ===
-memory_pool_t* memory_pool;// the used game memory pool
+// === Internal Global Variables ===
+memory_pool_t* mem_pool;
 
 /**
  * Frees all allocated resources and performs cleanup tasks for the game.
@@ -43,8 +43,8 @@ int init() {
     }
     create_tables_game_state(&db_connection);// only for dungeoncrawl_game.db
 
-    memory_pool = init_memory_pool(STANDARD_MEMORY_POOL_SIZE);
-    if (memory_pool == NULL) {
+    mem_pool = init_memory_pool(STANDARD_MEMORY_POOL_SIZE);
+    if (mem_pool == NULL) {
         log_msg(ERROR, "Main", "Failed to initialize memory pool");
         return FAIL_MEM_POOL_INIT;
     }
@@ -55,11 +55,11 @@ int init() {
 
     init_map_mode();
     init_main_menu();
-    if (init_combat_mode(memory_pool) != COMMON_SUCCESS) {
+    if (init_combat_mode(mem_pool) != COMMON_SUCCESS) {
         log_msg(ERROR, "Main", "Failed to initialize combat mode");
         return FAIL_GAME_MODE_INIT;
     }
-    if (init_game_data()) {
+    if (init_game_data(mem_pool) != COMMON_SUCCESS) {
         log_msg(ERROR, "Game", "Failed to initialize game components");
         return FAIL_GAME_ENTITY_INIT;
     }
@@ -75,8 +75,9 @@ void shutdown() {
     // close database connection in game.c
     db_close(&db_connection);
 
-    shutdown_combat_mode();
-    shutdown_memory_pool(memory_pool);
+    shutdown_combat_mode(mem_pool);
+    shutdown_memory_pool(mem_pool);
+
     shutdown_logger();
     tb_shutdown();
 }
