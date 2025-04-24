12849d9b90981630a25f3a5141cd5cdbdbb20e91
Nino <nino.frei@gmail.com>
2025-04-24 09:25:55 +0200

    Refactor game data management: integrate memory pool handling and improve null pointer checks

--- src/game_data.c
+++ src/game_data.c
@@ -2,60 +2,53 @@
 
 #include "character/monster.h"
 #include "character/player.h"
+#include "common.h"
 
 #include <stddef.h>
 
+// === Internal Global Variables ===
+memory_pool_t* mem_pool;
+
+// === External Global Variables ===
 ability_table_t* ability_table;
 character_t* goblin;
 character_t* player;
 potion_t* healing_potion;
 
-/**
- * Reset the goblin's state
- * @return 0 if successful, 1 if failed
- */
-int reset_goblin() {
-    goblin = create_new_goblin();
-    if (goblin == NULL) {
-        return 1;
-    } else {
-        add_ability(goblin, &ability_table->abilities[BITE]);
-    }
-    return 0;
-}
+int init_game_data(memory_pool_t* memory_pool) {
+    NULL_PTR_HANDLER_RETURN(memory_pool, 1, "Game Data", "Memory pool is NULL");
+    mem_pool = memory_pool;
 
-
-/**
- * Initialize game data
- * @return 0 if successful, 1 if failed
- */
-int init_game_data() {
-    ability_table = init_ability_table();
-    player = create_new_player();//initialize blank player
-    healing_potion = init_potion("Healing Potion", HEALING, 20);
+    ability_table = init_ability_table(mem_pool);
+    player = create_new_player(mem_pool);//initialize blank player
+    healing_potion = init_potion(mem_pool, "Healing Potion", HEALING, 20);
 
     reset_goblin();
 
-    if (ability_table == NULL || player == NULL || healing_potion == NULL) {
-        return 1;
-    } else {
-        // add abilities to player
-        add_ability(player, &ability_table->abilities[FIREBALL]);
-        add_ability(player, &ability_table->abilities[SWORD_SLASH]);
-        //add healing potion to player
-        add_potion(player, healing_potion);
-    }
+    if (ability_table == NULL || player == NULL || healing_potion == NULL) return 1;
+    // add abilities to player
+    add_ability(player, &ability_table->abilities[FIREBALL]);
+    add_ability(player, &ability_table->abilities[SWORD_SLASH]);
+    //add healing potion to a player
+    add_potion(player, healing_potion);
     return 0;
 }
 
-/**
- * Free game data
- */
 int free_game_data() {
-    free_ability_table(ability_table);
-    free_character(player);
-    free_character(goblin);
-    free_potion(healing_potion);
+    free_ability_table(mem_pool, ability_table);
+    free_character(mem_pool, player);
+    free_character(mem_pool, goblin);
+    free_potion(mem_pool, healing_potion);
+    return 0;
+}
 
+int reset_goblin() {
+    NULL_PTR_HANDLER_RETURN(mem_pool, 1, "Game Data", "Game Data was not initialized");
+    memory_pool_free(mem_pool, goblin);
+    goblin = create_new_goblin(mem_pool);
+    if (goblin == NULL) {
+        return 1;
+    }
+    add_ability(goblin, &ability_table->abilities[BITE]);
     return 0;
 }
--- src/game_data.h
+++ src/game_data.h
@@ -2,16 +2,26 @@
 #define GAME_DATA_H
 #include "combat/ability.h"
 #include "item/potion.h"
+#include "memory/memory_management.h"
 
 extern ability_table_t* ability_table;
 extern character_t* goblin;
 extern character_t* player;
 extern potion_t* healing_potion;
 
-int init_game_data();
-
+/**
+ * Initialize game data
+ * @return 0 if successful, 1 if failed
+ */
+int init_game_data(memory_pool_t* memory_pool);
+/**
+ * Free game data
+ */
 int free_game_data();
-
+/**
+ * Reset the goblin's state
+ * @return 0 if successful, 1 if failed
+ */
 int reset_goblin();
 
 #endif//GAME_DATA_H
