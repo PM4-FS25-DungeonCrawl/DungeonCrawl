0b1f1bed3703f1e4bac03ebc87708db1bf0eb734
Nino <nino.frei@gmail.com>
2025-05-12 13:16:26 +0200

    add documentation to stats_mode_local

--- src/stats/local/stats_mode_local.h
+++ src/stats/local/stats_mode_local.h
@@ -26,6 +26,20 @@ enum stats_mode_index {
 
 extern char** stats_mode_strings;
 
+/**
+ * Updates the localized strings used in the stats menu.
+ *
+ * This function refreshes the strings used to display various statistics
+ * or menu titles by retrieving their updated localized values. It first frees
+ * any previously allocated strings in the `stats_mode_strings` array and then
+ * populates the array with fresh localized strings retrieved from the
+ * `get_local_string` function.
+ *
+ * @note The function assumes that the `stats_mode_strings` array has
+ *       sufficient capacity to hold all the required strings (defined by
+ *       `MAX_STATS_STRINGS`). It also assumes that indices into the array
+ *       correspond to specific keys for localization.
+ */
 void update_stats_local(void);
 
 #endif//STATS_MODE_LOCAL_H
