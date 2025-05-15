f320cbe61949b62322d6d6f36d3edb91f494ab55
baranjak <ja.baranec@gmail.com>
2025-05-15 15:43:33 +0200

    TOODs and charater_database init

--- src/character/player.c
+++ src/character/player.c
@@ -13,6 +13,7 @@
 character_t* create_new_player(memory_pool_t* memory_pool) {
     NULL_PTR_HANDLER_RETURN(memory_pool, NULL, "Player", "Memory pool is NULL");
 
+    // TODO: Add a function to get the player name from the user (maybe add parameter char* name)
     character_t* player = init_character(memory_pool, PLAYER, "Hero");
     NULL_PTR_HANDLER_RETURN(player, NULL, "Player", "Failed to allocate memory for player");
 
@@ -22,3 +23,7 @@ character_t* create_new_player(memory_pool_t* memory_pool) {
     set_skill_points(player, 5);
     return player;
 }
+
+// TODO: function load_player(.. , db_connection, ...), which uses the function init_character(memory_pool, db_connection, ...)
+// TODO: The suggestion to implemet init_charater(memory_pool, db_connection, PLAYER, char* name) in charcter.c but up it's to you
+// campere with  init_charater with init_gear
---
+++ src/database/game/character_database.h
@@ -0,0 +1,6 @@
+#ifndef CHARACTER_DATABASE_H
+#define CHARACTER_DATABASE_H
+
+int save_character();
+
+#endif //CHARACTER_DATABASE_H
