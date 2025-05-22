c53484afaa0ee143708744b1af9e5f29f9a7700e
Tagger414 <tobi.zuelli@gmail.com>
2025-05-22 10:27:50 +0200

    update documentation item

--- src/item/loot_generation.h
+++ src/item/loot_generation.h
@@ -5,6 +5,14 @@
 #include "../item/gear.h"
 #include "../item/potion.h"
 
-void generate_loot(character_t* c, gear_table_t* gear_table, potion_table_t* potion_table, int rolls);
+/**
+ * @brief Generates loot for a character by randomly selecting a number of gear and potions.
+ *
+ * @param character The character to generate loot for.
+ * @param gear_table The table containing available gear.
+ * @param potion_table The table containing available potions.
+ * @param rolls The number of loot rolls to perform.
+ */
+void generate_loot(character_t* character, gear_table_t* gear_table, potion_table_t* potion_table, int rolls);
 
 #endif// LOOT_GENERATION_H
