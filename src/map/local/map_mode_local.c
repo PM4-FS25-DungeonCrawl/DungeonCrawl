544500db3818ad9e41e8bcf181f009252c4c434c
Nino <nino.frei@gmail.com>
2025-05-31 09:33:39 +0200

    add documentation for map mode local, and make update local function "private"

--- src/map/local/map_mode_local.c
+++ src/map/local/map_mode_local.c
@@ -7,6 +7,8 @@
 
 char** map_mode_strings = NULL;
 
+void update_map_mode_local(void);
+
 int init_map_mode_local(void) {
     map_mode_strings = malloc(sizeof(char*) * MAX_MAP_MODE_STRINGS);
     RETURN_WHEN_NULL(map_mode_strings, 1, "Map Mode Local", "Failed to allocate memory for map mode strings");
@@ -17,6 +19,18 @@ int init_map_mode_local(void) {
     update_map_mode_local();
     observe_local(update_map_mode_local);
     return 0;
+}
+
+void shutdown_map_mode_local(void) {
+    if (map_mode_strings != NULL) {
+        for (int i = 0; i < MAX_MAP_MODE_STRINGS; i++) {
+            if (map_mode_strings[i] != NULL) {
+                free(map_mode_strings[i]);
+            }
+        }
+        free(map_mode_strings);
+        map_mode_strings = NULL;
+    }
 }
 
 void update_map_mode_local(void) {
@@ -31,16 +45,4 @@ void update_map_mode_local(void) {
     map_mode_strings[PRESS_KEY_STATS] = get_local_string("MAP.PRESS.KEY.STATS");
     map_mode_strings[PRESS_KEY_INVENTORY] = get_local_string("MAP.PRESS.KEY.INVENTORY");
     map_mode_strings[PLAYER_POSITION_STR] = get_local_string("MAP.PLAYER.POSITION");
-}
-
-void shutdown_map_mode_local(void) {
-    if (map_mode_strings != NULL) {
-        for (int i = 0; i < MAX_MAP_MODE_STRINGS; i++) {
-            if (map_mode_strings[i] != NULL) {
-                free(map_mode_strings[i]);
-            }
-        }
-        free(map_mode_strings);
-        map_mode_strings = NULL;
-    }
 }
--- src/map/local/map_mode_local.h
+++ src/map/local/map_mode_local.h
@@ -9,12 +9,26 @@ enum map_mode_index {
     MAX_MAP_MODE_STRINGS
 };
 
+/**
+ * @var map_mode_strings
+ * Global array of local strings used by the map mode.
+ */
 extern char** map_mode_strings;
 
+/**
+ * @brief Initializes the local map mode.
+ *
+ * This function sets up any resources or state required for the local map mode.
+ *
+ * @return int Returns 0 on success, or a non-zero error code on failure.
+ */
 int init_map_mode_local(void);
 
-void update_map_mode_local(void);
-
+/**
+ * @brief Shuts down the local map mode.
+ *
+ * This function releases any resources or state associated with the local map mode.
+ */
 void shutdown_map_mode_local(void);
 
 #endif//MAP_MODE_LOCAL_H
